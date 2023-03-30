0376c9339da62cc51e4a391d34ae6823d013c702
juAlberti <julia.maia@acad.pucrs.br>
2023-03-30 02:42:41 -0300

    save grocery itens is working

--- CloudKitUIKitStudies/CloudKitUIKitStudies/ViewController.swift
+++ CloudKitUIKitStudies/CloudKitUIKitStudies/ViewController.swift
@@ -4,16 +4,66 @@
 //
 //  Created by Julia Alberti Maia on 10/02/23.
 //
-
+import CloudKit
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+ 
+    let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+    
+    private let database = CKContainer(identifier: "iCloud.com.example.CloudKitUIKitStudies").publicCloudDatabase
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Grocery List"
+        view.addSubview(tableView)
+        tableView.dataSource = self
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add,
+                                                            target: self,
+                                                            action: #selector(didTapAdd))
+    }
+    
+    @objc func didTapAdd() {
+        let alert = UIAlertController(title: "Add Item", message: nil, preferredStyle: .alert)
+        alert.addTextField { field in
+            field.placeholder = "Enter name..."
+        }
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Add", style: .default, handler: { [weak self] _ in
+            if let field = alert.textFields?.first, let text = field.text, !text.isEmpty {
+                self?.saveItem(name: text)
+            }
+        }))
+        present(alert,animated: true)
+    }
+    
+    @objc func saveItem(name: String) {
+        let record = CKRecord(recordType: "GroceryItem")
+        record.setValue(name, forKey: "name")
+        database.save(record) { record, error in
+            if record != nil, error == nil {
+                print("saved")
+            }
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Item"
+        return cell
+    }
 }
 
